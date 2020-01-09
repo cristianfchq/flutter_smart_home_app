76cbf84611ab93b79cb9f8956b1e08a91d03c45c
cristianfchq <cristian.fchq@gmail.com>
2020-01-09 13:07:07 -0400

    dando color al bottonBar

--- lib/bottom-bar.dart
+++ lib/bottom-bar.dart
@@ -13,95 +13,98 @@ class _BottomBarState extends State<BottomBar> {
   Widget build(BuildContext context) {
     double itemWidth = MediaQuery.of(context).size.width / 5;
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          width: itemWidth,
-          alignment: Alignment.center,
-          child: Ink(
-            child: InkWell(
-              onTap: (){},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.home,
-                    color: color,
-                    size: 35,
-                  ),
-                  Text("Home")
-                ],
+    return Container(
+      color: Colors.yellow[200],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: itemWidth,
+            alignment: Alignment.center,
+            child: Ink(
+              child: InkWell(
+                onTap: (){},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.home,
+                      color: color,
+                      size: 35,
+                    ),
+                    Text("Home")
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Container(
-          width: itemWidth,
-          alignment: Alignment.center,
-          child: Ink(
-            child: InkWell(
-              onTap: (){},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.forward_5,
-                    color: Colors.grey,
-                    size: 35,
-                  ),
-                  Text("Routines")
-                ],
+          Container(
+            width: itemWidth,
+            alignment: Alignment.center,
+            child: Ink(
+              child: InkWell(
+                onTap: (){},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.forward_5,
+                      color: Colors.grey,
+                      size: 35,
+                    ),
+                    Text("Routines")
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Container(
-          width: itemWidth,
+          Container(
+            width: itemWidth,
 
-        ),
-        Container(
-          width: itemWidth,
-          alignment: Alignment.center,
-          child: Ink(
-            child: InkWell(
-              onTap: (){},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.show_chart,
-                    color: Colors.grey,
-                    size: 35,
-                  ),
-                  Text("Stats")
-                ],
+          ),
+          Container(
+            width: itemWidth,
+            alignment: Alignment.center,
+            child: Ink(
+              child: InkWell(
+                onTap: (){},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.show_chart,
+                      color: Colors.grey,
+                      size: 35,
+                    ),
+                    Text("Stats")
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Container(
-          width: itemWidth,
-          alignment: Alignment.center,
-          child: Ink(
-            child: InkWell(
-              onTap: (){},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.menu,
-                    color: Colors.grey,
-                    size: 35,
-                  ),
-                  Text("More")
-                ],
+          Container(
+            width: itemWidth,
+            alignment: Alignment.center,
+            child: Ink(
+              child: InkWell(
+                onTap: (){},
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.menu,
+                      color: Colors.grey,
+                      size: 35,
+                    ),
+                    Text("More")
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
